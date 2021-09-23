481a7d69c0eefc42e2839ea48b03c1c2f1d8db02
sanyavertolet <lxnfrolov@gmail.com>
2021-09-23 22:50:32 +0300

    Third task is done!

---
+++ 20210923/3/task3.py
@@ -0,0 +1,17 @@
+n = int(input())
+i = 0
+while i < 3:
+    j = 0
+    while j < 3:
+        a = (n + i) * (n + j)
+        s = 0
+        while a != 0:
+            s += a % 10
+            a //= 10
+        if s % 6 == 0:
+            print(n + i, '*', n + j, '=:=)', sep='', end=' ')
+        else:
+            print(n + i, '*', n + j, '=', (n + i) * (n + j), sep='', end=' ')
+        j += 1
+    i += 1
+    print('')
