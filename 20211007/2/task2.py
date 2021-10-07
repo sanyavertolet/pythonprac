71cec00382dc3860ee46030f33742fedfdcd5384
sanyavertolet <lxnfrolov@gmail.com>
2021-10-07 15:53:16 +0300

    Second task is done!

---
+++ 20211007/2/task2.py
@@ -0,0 +1,7 @@
+def SUB(a, b):
+    if '__sub__' in dir(a):
+        return a - b
+    return type(a)([item for item in a if item not in b]) if type(a) != type('') else ''.join([item for item in a if item not in b])
+
+
+print(SUB(*eval(input())))
