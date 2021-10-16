d3129dbd63e51d913691d747f64d70ad2f2cdf67
sanyavertolet <lxnfrolov@gmail.com>
2021-10-17 00:41:15 +0300

    Second task is done!

--- 20211014/2/task2.py
+++ 20211014/2/task2.py
@@ -1,20 +1,47 @@
-from math import sin
+from math import *
 
 def scale(A, B, a, b, x):
     return (x - A) / (B - A) * (b - a) + a
 
-f = sin
+def function(s):
+    def f(x):
+        return eval(s)
+    return f
 
-width = 80
-height = 25
+data = input().split()
 
-x_from, x_to = -4, 4
-y_from, y_to = -1, 1
+width, height = int(data[0]), int(data[1])
+x_from, x_to = float(data[2]), float(data[3])
+s = ''.join(data[4:])
+f = function(s)
+X = [(x, scale(0, width + 1, x_from, x_to, x)) for x in range(width + 1)]
+F = [(x[0], f(x[1])) for x in X]
 
-X = [scale(0, width + 1, x_from, x_to, x) for x in range(width + 1)]
-Y = [f(x) for x in X]
+y_from = F[0][1]
+y_to = F[0][1]
 
-min_y, max_y = min(Y), max(Y)
+for y in F:
+    if y[1] > y_to:
+        y_to = y[1]
+    if y[1] < y_from:
+        y_from = y[1]
 
-for y in Y:
-    print(int(scale(min_y, max_y, 0, width, y)) * " " + "*")
+F = [(y[0], int(scale(y_to, y_from, 0, height + 1, y[1]))) for y in F]
+
+field = [[' ' for j in range(width + 2)] for i in range(height + 2)]
+
+
+for i in range(len(F)):
+    field[F[i][1]][F[i][0]] = '*'
+    if i != 0:
+        prev_point = F[i - 1]
+        for j in range(min(F[i - 1][1], F[i][1]) + 1, max(F[i - 1][1], F[i][1])):
+            field[j][F[i - 1][0]] = '*'
+
+
+field = [''.join(raw) for raw in field]
+
+for raw in field:
+    print(raw)
+
+#print(*field)
