841f110afbb51bf29c71c7daa8a1d17a432f051f
sanyavertolet <lxnfrolov@gmail.com>
2021-10-24 15:11:58 +0300

    Second task is done!

--- 20211021/2/task2.py
+++ 20211021/2/task2.py
@@ -1,15 +1,20 @@
 import math
 
-#TODO: update
+f = dict() # f[fname] = (var1, var2, ..., expr)
 
-f = dict()
-
+counter = 1
 command = input()
 while command.split()[0] != 'quit':
+    counter += 1
     lexems = command.split()
-    if len(lexems) == 3:
-        f[lexems[0][1:]] = (lexems[1], lexems[2])
+    if lexems[0][0] == ':':
+        f[lexems[0][1:]] = lexems[1:]
     else:
-        print(eval(f[lexems[0]][1], math.__dict__, { f[lexems[0]][0]: eval(lexems[1]) }))
+        fname = lexems[0]
+        val = lexems[1:]
+        var = f[fname][:len(f[fname]) - 1]
+        expr = f[fname][-1]
+        var_dict = { vr: eval(vl) for vr, vl in zip(var, val)}
+        print(eval(expr, math.__dict__, var_dict))
     command = input()
-print(len(f) + 1)
+print(command.split()[1].format(len(f) + 1, counter))
