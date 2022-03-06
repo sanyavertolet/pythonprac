51e01c9164767f6aecb00d01134bfc27b36b5422
sanyavertolet <lxnfrolov@gmail.com>
2022-03-06 11:51:56 +0300

    Improved dist(s1, s2) function
    Now dist() has third argument s3 so that function looks like `dist(s1,
    s2, s3)`

--- 20220228/1/program.py
+++ 20220228/1/program.py
@@ -1,7 +1,7 @@
 import textdistance
 
 
-def dist(s1, s2):
+def dist(s1, s2, s3):
     return textdistance.levenshtein(s1, s2)
 
 
@@ -15,5 +15,5 @@ s1 = get_valid_string()
 s2 = get_valid_string()
 s3 = get_valid_string()
 
-res = dist(s1, s2)
+res = dist(s1, s2, s3)
 
