f856aadc5f2fe90db03e6599b9d1ff24be44f7ca
sanyavertolet <lxnfrolov@gmail.com>
2022-03-14 03:23:32 +0300

    Improved previous solution but not tested yet

--- 20220314/2/antip.py
+++ 20220314/2/antip.py
@@ -4,41 +4,20 @@ import re
 import sys
 import textdistance
 
-def get_usrc(fname):
+def get_utree(fname):
     with open(fname) as f:
-        return ast.unparse(ast.parse(f.read()))
-    
-def get_tree(node):
-    ast_tree = ast.dump(ast.NodeTransformer().generic_visit(node), annotate_fields=False)
-    return ast_tree
-
-def resub(src):
-    #src = re.sub()
+        src = ast.dump(ast.NodeTransformer().generic_visit(ast.parse(ast.unparse(ast.parse(f.read())))), annotate_fields=False)
     src = re.sub('\s*', '', src)
     src = re.sub('\'[^\']*\'', '', src)
     src = re.sub('\'\'\'[^\'\'\']*\'\'\'', '', src)
     src = re.sub('\"[^\"]*\"', '', src)
     src = re.sub('\"\"\"[^\"\"\"]*\"\"\"', '', src)
     src = re.sub('[A-Z]([^(\(|\[)])*\([^A-Z\[\()\],]*', '`(', src)
-    src = re.sub('[A-Z]([^(\(|\[)])*\[[^A-Z\[\()\],]*', '`[', src)
-    return src
+    return re.sub('[A-Z]([^(\(|\[)])*\[[^A-Z\[\()\],]*', '`[', src)
 
 if len(sys.argv) == 3:
-    src1 = get_usrc(sys.argv[1])
-    src2 = get_usrc(sys.argv[2])
-    
-    src1 = get_tree(ast.parse(src1))
-    src2 = get_tree(ast.parse(src2))
-    
-    src1 = resub(src1)
-    src2 = resub(src2)
-
-    ratio = textdistance.damerau_levenshtein.normalized_distance(src1, src2)
-    print(ratio)
-    if ratio < 0.1:
-        print('Stolen!!')
-    else:
-        print('Not stolen.')
+    src = [get_utree(sys.argv[i]) for i in range(1, len(sys.argv))]
+    print('Stolen!!' if textdistance.damerau_levenshtein.normalized_distance(*src) < 0.1 else 'Not stolen.')
 else:
     print('Usage: antip.py file1.py file2.py')
 
